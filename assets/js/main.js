a21600bbb0f65f20ddc19f8415d94aaa3bce0082
Codetari.ID | Fullstack Web Developer, AI Prompter, Desktop Dev (VB.NET, Java, Python) <idcodetari@gmail.com>
2025-09-02 21:13:07 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -218,3 +218,14 @@
   document.addEventListener('scroll', navmenuScrollspy);
 
 })();
+
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
+
+document.addEventListener('keydown', function(e) {
+    if(e.key === 'F12') e.preventDefault();
+    if(e.ctrlKey && e.shiftKey && e.key === 'I') e.preventDefault();
+    if(e.ctrlKey && e.shiftKey && e.key === 'J') e.preventDefault();
+    if(e.ctrlKey && e.key === 'U') e.preventDefault();
+});
